2586e550c4bcadd15acea689181781df3d54ba10
Masiss <hopelessornohope@gmail.com>
2022-07-21 07:23:01 +0700

    update Homepage lần 1

---
+++ resources/views/homepage_layout/nav_bar.blade.php
@@ -0,0 +1,110 @@
+<nav
+    class="header-navbar navbar navbar-expand-lg align-items-center floating-nav navbar-light navbar-shadow container-xxl"
+    style="width: calc(100vw - (100vw - 100%) - calc(2rem * 2) - 25%); margin-right: 2em;">
+    <div class="navbar-container d-flex content">
+        <div class="bookmark-wrapper d-flex align-items-center">
+
+            <ul class="nav navbar-nav d-xl-none">
+                <li class="nav-item">
+                    <a class="nav-link menu-toggle" href="#">
+                        <i class="ficon" data-feather="menu">
+
+                        </i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <ul class="nav navbar-nav align-items-center ms-auto">
+            <li class="nav-item nav-search">
+                <a class="nav-link nav-link-search">
+                    <i class="ficon" data-feather="search">
+
+                    </i>
+                </a>
+                <div class="search-input">
+                    <div class="search-input-icon">
+                        <i data-feather="search">
+
+                        </i>
+                    </div>
+                    <input class="form-control input" type="text" placeholder="Tìm kiếm" tabindex="-1"
+                           data-search="search">
+                    <div class="search-input-close">
+                        <i data-feather="x">
+
+                        </i>
+                    </div>
+                    <ul class="search-list search-list-main">
+
+                    </ul>
+                </div>
+            </li>
+
+
+            <li class="nav-item dropdown dropdown-user">
+                <a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="#"
+                   data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+                    @if(auth()->guard('instructor')->check() || auth()->guard('driver')->check())
+                        <div class="user-nav d-sm-flex d-none">
+                            <span class="user-name fw-bolder">Name</span>
+                            <span class="user-status">Role</span>
+                        </div>
+                        <span class="avatar"> <img class="round" src="" alt="avatar" height="40" width="40"> <span
+                                class="avatar-status-online"></span></span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-user">
+                    <a class="dropdown-item"
+                       href="page-profile.html">
+                        <i class="me-50" data-feather="user"> </i> Profile</a>
+                    <a class="dropdown-item" href="app-email.html">
+                        <i class="me-50" data-feather="mail"> </i>
+                        Inbox
+                    </a>
+                    <a class="dropdown-item" href="app-todo.html">
+                        <i class="me-50" data-feather="check-square"> </i>
+                        Task
+                    </a>
+                    <a class="dropdown-item" href="app-chat.html">
+                        <i class="me-50" data-feather="message-square"> </i>
+                        Chats
+                    </a>
+                    <div class="dropdown-divider">
+
+                    </div>
+                    <a class="dropdown-item" href="page-account-settings-account.html">
+                        <i class="me-50" data-feather="settings"> </i>
+                        Settings
+                    </a>
+                    <a class="dropdown-item" href="page-pricing.html">
+                        <i class="me-50" data-feather="credit-card"> </i>
+                        Pricing
+                    </a>
+                    <a class="dropdown-item" href="page-faq.html">
+                        <i class="me-50" data-feather="help-circle"> </i>
+                        FAQ
+                    </a>
+                    <a class="dropdown-item" href="{{route('logout')}}">
+                        <i class="me-50" data-feather="power"> </i>
+                        Logout
+                    </a>
+                </div>
+                @elseif(auth()->guest())
+                    <div class="card-content">
+                        <a href="{{route('register')}}">
+                            <span style="color: #606C80">
+                                Đăng ký
+                            </span>
+                        </a>
+                        /
+                        <a href="{{route('login')}}">
+                            <span style="color: #606C80">
+                                Đăng nhập
+                            </span>
+                        </a>
+                    </div>
+                @endif
+            </li>
+        </ul>
+    </div>
+</nav>
---
+++ resources/views/homepage_layout/sidebar.blade.php
@@ -0,0 +1,75 @@
+<div class="main-menu  menu-accordion " data-scroll-to-active="true" style="width: 25%">
+    <div class="navbar-header m-3">
+        <a class="navbar-brand" href="/index">
+            <img src="{{asset('logo.svg')}}" width="" class="logo">
+        </a>
+    </div>
+    <div class="main-menu-content mt-5 me-1">
+        <ul class="navigation navigation-main list-group" id="main-menu-navigation" data-menu="menu-navigation">
+            <li>
+                <a class="sidebar-list">
+                    <i data-feather="grid"></i>
+                    <span>Trang chủ</span>
+                </a>
+            </li>
+            <li>
+                <a class="sidebar-list">
+                    <i data-feather="message-square"></i>
+                    <span>Liên hệ</span>
+                </a>
+            </li>
+            <li>
+                <a class="sidebar-list">
+                    <i data-feather="calendar"></i>
+                    <span>Các khóa học</span>
+                </a>
+            </li>
+            <li>
+                <a class="sidebar-list">
+                    <i data-feather="book"></i>
+                    <span>Tài liệu</span>
+                </a>
+            </li>
+            <li>
+                <a class="sidebar-list">
+                    <i data-feather="info"></i>
+                    <span>Blog</span>
+                </a>
+            </li>
+
+        </ul>
+        <form class="form-control">
+            <div class="card"
+                 style="background: linear-gradient(180deg, rgba(58, 97, 238, 0.693) 0%, rgba(44, 45, 89, 0) 130.73%);">
+                <div class=" text-center card-header" style="color: #FFFFFF">Đăng ký tư vấn</div>
+                <div class="row d-flex justify-content-center ">
+                    <div>
+                        <input type="text" style=" background: #DFDADAAB; " class="form-control m-1 w-auto" name="name"
+                               placeholder="Họ và tên">
+                    </div>
+                    <div>
+                        <input type="text" style=" background: #DFDADAAB;" class="form-control m-1 w-auto"
+                               name="phone_numbers" placeholder="Số điện thoại">
+                    </div>
+                    <div>
+                        <input type="text" style=" background: #DFDADAAB;" class="form-control m-1 w-auto" name="email"
+                               placeholder="Email">
+                    </div>
+                    <div class="w-auto">
+                        <label style="color: #FFFFFF">Thời gian có thể nhận tư vấn</label>
+                        <select>
+                            @for($i=6;$i<24;$i++)
+                                <option value="{{$i}}">{{$i}}</option>
+                            @endfor
+                        </select>
+                    </div>
+                    <div class=" d-flex">
+                        <button class="btn " style="color: #FFFFFF">Đăng ký</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+
+    </div>
+
+</div>

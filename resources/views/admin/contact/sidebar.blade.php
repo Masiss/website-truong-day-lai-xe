feea0a21c2f5c5f0f5ab73f7b52facdff43d5eba
Binh Nguyen <hopelessornohope@gmail.com>
2025-11-07 16:29:49 +0700

    fix ui

--- resources/views/admin/contact/sidebar.blade.php
+++ resources/views/admin/contact/sidebar.blade.php
@@ -9,11 +9,13 @@
                     <div class="list-group list-group-labels">
                         @foreach($types as $type)
                             <button name="btn-type" data-type="{{$type->value}}" href="#" class="list-group-item list-group-item-action"
-                            ><span class="bullet bullet-sm bullet-primary me-1"></span>{{\App\Enums\TypeContactEnums::toVNese($type->value)}}</button
+                            >
+                            {{\App\Enums\TypeContactEnums::toVNese($type->value)}}</button
                             >
                         @endforeach
                         <button name="btn-type"  href="#" class="list-group-item list-group-item-action"
-                        ><span class="bullet bullet-sm bullet-primary me-1"></span>Tất cả</button
+                        >
+                            Tất cả</button
                         >
                     </div>
                 </div>
